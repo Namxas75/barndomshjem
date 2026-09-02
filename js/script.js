ac669cabdb385fd65da081fd0d2f9e85954eb297
Troels Busk-Jepsen <troelsbusk@gmail.com>
2026-09-02 21:48:40 +0200

    Prepare app for GitHub Pages

--- js/script.js
+++ js/script.js
@@ -1,4 +1,29 @@
 (function(){
+	document.addEventListener('keydown', function(e){
+		if (e.ctrlKey || e.metaKey || e.altKey) {
+			return;
+		}
+
+		var keyDirections = {
+			37: -1,
+			38: 0,
+			39: 1,
+			ArrowLeft: -1,
+			ArrowUp: 0,
+			ArrowRight: 1
+		};
+		var pressedKey = e.key || e.which || e.keyCode;
+		var direction = keyDirections[pressedKey];
+
+		if (direction !== undefined) {
+			e.preventDefault();
+			var arrow = document.querySelector('a.arrow[data-direction="' + direction + '"]');
+			if (arrow) {
+				arrow.click();
+			}
+		}
+	});
+
 	$.getJSON('json/home.json').done(function(data){
 
 		var dir = 2;
@@ -18,19 +43,19 @@
 			switch (new_direction) {
 				case -1:
 					$img.effect('drop',{direction:'right'},200,function(){
-						$img.attr("src","/images/" + view['image']).effect('slide', {direction: 'left'},'show', 350);
+						$img.attr("src","images/" + view['image']).effect('slide', {direction: 'left'},'show', 350);
 					});
 				break;
 				case 1:
 					$img.effect('drop',{direction:'left'},200,function(){
-						$img.attr("src","/images/" + view['image']).effect('slide', {direction: 'right'},'show', 350);
+						$img.attr("src","images/" + view['image']).effect('slide', {direction: 'right'},'show', 350);
 					});
 				break;
 				case 0:
 					node = view['link'];
 					view = data[node][dir];
 					$img.effect('fade',function(){
-						$img.attr("src","/images/" + view['image']).fadeIn();
+						$img.attr("src","images/" + view['image']).fadeIn();
 					});
 				break;
 			}
